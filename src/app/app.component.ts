9ab6d5727b0df226bc50c1066558033c4703df98
Carracedo <marinacarracedo14@gmail.com>
2024-01-22 17:32:30 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: '../styles.css'
 })
-export class AppComponent {
-  title = 'granja';
+export class AppComponent{
+    title = 'granja';
+    // miGranja: any;
+
+    // constructor(private datosMiGranja: miGranjaService){}
+
+    // ngOnInit(): void {
+    //   this.datosMiGranja.obtenerDatos().subscribe(data  => {
+    //     console.log(data);
+    //   this.datosMiGranja=data;
+    // });
+    // }
 }
+
