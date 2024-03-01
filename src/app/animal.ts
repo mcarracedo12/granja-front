c6fe69cd5b77e41e33364a5d4f03c9dabdee613e
Carracedo <marinacarracedo14@gmail.com>
2024-03-01 14:54:54 -0300

    Trae los datos a consola pero no puedo hacer ng for en Tipos, Compras y Ventas

--- src/app/animal.ts
+++ src/app/animal.ts
@@ -2,9 +2,18 @@
 export interface animal
 {
     id?: number,
-    edadActual: number, 
-    precioVentaByTipo: number,
-    precioCompraByTipo: number
-    
-    
+    fechaIngresoAGranja :  Date,
+    edadEnDiasAlIngresar : number,
+    nacimiento :  Date ,
+    fechaExpiracion :  Date ,
+//  precioCompra : 0.0,
+//  precioVenta : 0.0,
+    tiempoDeReproduccionByTipo : number,
+    diasExpiracionByTipo : number,
+//  precioCompraByTipo : 10.0,
+//  cantidadMaximaByTipo : 10000,
+//  precioVentaByTipo : number,
+    edadActual: number,
+    tipoId: number,
+    animalByTipo:  string 
 }
